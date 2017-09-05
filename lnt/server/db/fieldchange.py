llvm-mirror/lnt

commit dcab10c847636c460bb01e47e4e07ab1f5cd4118

Keep sqlalchemy session separate from database objects
SQLAlchemy sessions should be handled independently of the lifetimes of
database objects.

See also the SQLAlchemy documentation:
http://docs.sqlalchemy.org/en/latest/orm/session_basics.html esp. the
"When do I construct a Session, when do I commit it, and when do I close
it?" part: "As a general rule, keep the lifecycle of the session
separate and external from functions and objects that access and/or
manipulate database data. This will greatly help with achieving a
predictable and consistent transactional scope."

This requires a bunch of mechanical code changes to make sure the
session object is passed around separately and a session needs to be
created explicitely now rather than getting a long lived session as part
of V4DB.

Advantages:
- We can construct the DB object and the testsuite table descriptions
  once when LNT starts. This avoids a bunch of database queries and
  sqlalchemy model building logic running again for each request.
- We can switch session.rollback() to session.close() in an upcoming
  commit, avoid countless unnecessary database queries happening on
  rollback().
- Avoids a bunch of obscure caching effects with long lived sessions
  helping to clean up LNT code (the cleanup already happened in r312059)

Differential Revision: https://reviews.llvm.org/D37360

git-svn-id: https://llvm.org/svn/llvm-project/lnt/trunk@312363 91177308-0d34-0410-b5e6-96231b3b80d8

lnt/server/db/fieldchange.py
@@ -15,16 +15,16 @@ from lnt.server.db import rules_manager as rules
 FIELD_CHANGE_LOOKBACK = 10
 
 
-def post_submit_tasks(ts, run_id):
-    regenerate_fieldchanges_for_run(ts, run_id)
+def post_submit_tasks(session, ts, run_id):
+    regenerate_fieldchanges_for_run(ts, session, run_id)
 
 
-def delete_fieldchange(ts, change):
+def delete_fieldchange(session, ts, change):
     """Delete this field change.  Since it might be attahed to a regression
     via regression indicators, fix those up too.  If this orphans a regression
     delete it as well."""
     # Find the indicators.
-    indicators = ts.query(ts.RegressionIndicator). \
+    indicators = session.query(ts.RegressionIndicator). \
         filter(ts.RegressionIndicator.field_change_id == change.id). \
         all()
     # And all the related regressions.
@@ -32,43 +32,45 @@ def delete_fieldchange(ts, change):
 
     # Remove the idicators that point to this change.
     for ind in indicators:
-        ts.delete(ind)
+        session.delete(ind)
 
     # Now we can remove the change, itself.
-    ts.delete(change)
+    session.delete(change)
 
     # We might have just created a regression with no changes.
     # If so, delete it as well.
     deleted_ids = []
     for r in regression_ids:
-        remaining = ts.query(ts.RegressionIndicator). \
+        remaining = session.query(ts.RegressionIndicator). \
             filter(ts.RegressionIndicator.regression_id == r). \
             all()
         if len(remaining) == 0:
-            r = ts.query(ts.Regression).get(r)
+            r = session.query(ts.Regression).get(r)
             logger.info("Deleting regression because it has not changes:" +
                         repr(r))
-            ts.delete(r)
+            session.delete(r)
             deleted_ids.append(r)
-    ts.commit()
+    session.commit()
     return deleted_ids
 
 
 @timed
-def regenerate_fieldchanges_for_run(ts, run_id):
+def regenerate_fieldchanges_for_run(ts, session, run_id):
     """Regenerate the set of FieldChange objects for the given run.
     """
     # Allow for potentially a few different runs, previous_runs, next_runs
     # all with the same order_id which we will aggregate together to make
     # our comparison result.
-    run = ts.getRun(run_id)
-    runs = ts.query(ts.Run). \
+    run = ts.getRun(session, run_id)
+    runs = session.query(ts.Run). \
         filter(ts.Run.order_id == run.order_id). \
         filter(ts.Run.machine_id == run.machine_id). \
         all()
 
-    previous_runs = ts.get_previous_runs_on_machine(run, FIELD_CHANGE_LOOKBACK)
-    next_runs = ts.get_next_runs_on_machine(run, FIELD_CHANGE_LOOKBACK)
+    previous_runs = ts.get_previous_runs_on_machine(session, run,
+                                                    FIELD_CHANGE_LOOKBACK)
+    next_runs = ts.get_next_runs_on_machine(session, run,
+                                            FIELD_CHANGE_LOOKBACK)
 
     # Find our start/end order.
     if previous_runs != []:
@@ -90,7 +92,7 @@ def regenerate_fieldchanges_for_run(ts, run_id):
     if run_size > 50:
         logger.warning("Generating field changes for {} runs."
                        "That will be very slow.".format(run_size))
-    runinfo = lnt.server.reporting.analysis.RunInfo(ts, runs_to_load)
+    runinfo = lnt.server.reporting.analysis.RunInfo(session, ts, runs_to_load)
 
     # Only store fieldchanges for "metric" samples like execution time;
     # not for fields with other data, e.g. hash of a binary
@@ -102,12 +104,12 @@ def regenerate_fieldchanges_for_run(ts, run_id):
                 ts.Sample.get_hash_of_binary_field())
             # Try and find a matching FC and update, else create one.
             try:
-                f = ts.query(ts.FieldChange) \
+                f = session.query(ts.FieldChange) \
                     .filter(ts.FieldChange.start_order == start_order) \
                     .filter(ts.FieldChange.end_order == end_order) \
                     .filter(ts.FieldChange.test_id == test_id) \
                     .filter(ts.FieldChange.machine == run.machine) \
-                    .filter(ts.FieldChange.field == field) \
+                    .filter(ts.FieldChange.field_id == field.id) \
                     .one()
             except sqlalchemy.orm.exc.NoResultFound:
                 f = None
@@ -115,25 +117,29 @@ def regenerate_fieldchanges_for_run(ts, run_id):
             if not result.is_result_performance_change() and f:
                 # With more data, its not a regression. Kill it!
                 logger.info("Removing field change: {}".format(f.id))
-                deleted = delete_fieldchange(ts, f)
+                deleted = delete_fieldchange(session, ts, f)
                 continue
 
             if result.is_result_performance_change() and not f:
-                test = ts.query(ts.Test).filter(ts.Test.id == test_id).one()
+                test = session.query(ts.Test) \
+                    .filter(ts.Test.id == test_id) \
+                    .one()
                 f = ts.FieldChange(start_order=start_order,
                                    end_order=run.order,
                                    machine=run.machine,
                                    test=test,
-                                   field=field)
+                                   field_id=field.id)
                 # Check the rules to see if this change matters.
-                if rules.is_useful_change(ts, f):
-                    ts.add(f)
+                if rules.is_useful_change(session, ts, f):
+                    session.add(f)
                     try:
-                        found, new_reg = identify_related_changes(ts, f)
+                        found, new_reg = identify_related_changes(session, ts,
+                                                                  f)
                     except ObjectDeletedError:
                         # This can happen from time to time.
                         # So, lets retry once.
-                        found, new_reg = identify_related_changes(ts, f)
+                        found, new_reg = identify_related_changes(session, ts,
+                                                                  f)
 
                     if found:
                         logger.info("Found field change: {}".format(
@@ -144,10 +150,10 @@ def regenerate_fieldchanges_for_run(ts, run_id):
                 f.old_value = result.previous
                 f.new_value = result.current
                 f.run = run
-    ts.commit()
+    session.commit()
 
-    regressions = ts.query(ts.Regression).all()[::-1]
-    rules.post_submission_hooks(ts, regressions)
+    regressions = session.query(ts.Regression).all()[::-1]
+    rules.post_submission_hooks(session, ts, regressions)
 
 
 def is_overlaping(fc1, fc2):
@@ -176,7 +182,7 @@ def percent_similar(a, b):
 
 
 @timed
-def identify_related_changes(ts, fc):
+def identify_related_changes(session, ts, fc):
     """Can we find a home for this change in some existing regression? If a
     match is found add a regression indicator adding this change to that
     regression, otherwise create a new regression for this change.
@@ -185,14 +191,14 @@ def identify_related_changes(ts, fc):
     ranges. Then looks for changes that are similar.
 
     """
-    regressions = ts.query(ts.Regression.id) \
+    regressions = session.query(ts.Regression.id) \
         .filter(or_(ts.Regression.state == RegressionState.DETECTED,
                 ts.Regression.state == RegressionState.DETECTED_FIXED)) \
         .all()
 
     for regression_packed in regressions:
         regression_id = regression_packed[0]
-        regression_indicators = get_ris(ts, regression_id)
+        regression_indicators = get_ris(session, ts, regression_id)
         print "RIs:", regression_indicators
         for change in regression_indicators:
             regression_change = change.field_change
@@ -204,21 +210,22 @@ def identify_related_changes(ts, fc):
                 confidence += percent_similar(regression_change.test.name,
                                               fc.test.name)
 
-                if regression_change.field == fc.field:
+                if regression_change.field_id == fc.field_id:
                     confidence += 1.0
 
                 if confidence >= 2.0:
                     # Matching
                     MSG = "Found a match: {} with score {}."
-                    regression = ts.query(ts.Regression).get(regression_id)
+                    regression = session.query(ts.Regression) \
+                        .get(regression_id)
                     logger.info(MSG.format(str(regression),
                                            confidence))
                     ri = ts.RegressionIndicator(regression, fc)
-                    ts.add(ri)
+                    session.add(ri)
                     # Update the default title if needed.
-                    rebuild_title(ts, regression)
-                    ts.commit()
+                    rebuild_title(session, ts, regression)
+                    session.commit()
                     return True, regression
     logger.info("Could not find a partner, creating new Regression for change")
-    new_reg = new_regression(ts, [fc.id])
+    new_reg = new_regression(session, ts, [fc.id])
     return False, new_reg

lnt/util/async_ops.py
@@ -106,6 +106,7 @@ def async_run_job(job, db_name, ts, func_args, db_config):
     job.start()
     JOBS.append(job)
 
+
 # Flag to track if we have disposed of the parents database connections in
 # this subprocess.
 clean_db = False
@@ -124,15 +125,18 @@ def async_wrapper(job, ts_args, func_args):
         if not clean_db:
             lnt.server.db.v4db.V4DB.close_all_engines()
             clean_db = True
+
         sleep(3)
         logger.info("Running async wrapper: {} ".format(job.__name__) +
                     str(os.getpid()))
         config = ts_args['db_info']
-        _v4db = config.get_database(ts_args['db'])
-        # with contextlib.closing(_v4db) as db:
-        ts = _v4db.testsuite[ts_args['tsname']]
-        nothing = job(ts, **func_args)
-        assert nothing is None
+        db = config.get_database(ts_args['db'])
+        with contextlib.closing(db):
+            session = db.make_session()
+            ts = db.testsuite[ts_args['tsname']]
+            nothing = job(ts, session, **func_args)
+            assert nothing is None
+            session.close()
         end_time = time.time()
         delta = end_time-start_time
         msg = "Finished: {name} in {time:.2f}s ".format(name=job.__name__,

lnt/lnttool/import_data.py
@@ -42,11 +42,12 @@ def action_import(instance_path, files, database, output_format, show_sql,
 
     # Get the database.
     with contextlib.closing(config.get_database(database)) as db:
+        session = db.make_session()
         # Load the database.
         success = True
         for file_name in files:
             result = lnt.util.ImportData.import_and_report(
-                config, database, db, file_name,
+                config, database, db, session, file_name,
                 output_format, testsuite, show_sample_count, no_email,
                 no_report, updateMachine=update_machine, mergeRun=merge)
 

lnt/lnttool/main.py
@@ -92,9 +92,10 @@ def action_checkformat(files, testsuite):
     import lnt.util.ImportData
     db = lnt.server.db.v4db.V4DB('sqlite:///:memory:',
                                  lnt.server.config.Config.dummy_instance())
+    session = db.make_session()
     for file in files:
         result = lnt.util.ImportData.import_and_report(
-            None, None, db, file, '<auto>', testsuite)
+            None, None, db, session, file, '<auto>', testsuite)
         lnt.util.ImportData.print_report_result(result, sys.stdout,
                                                 sys.stderr, verbose=True)
 
@@ -243,6 +244,7 @@ def action_send_daily_report(instance_path, address, database, testsuite, host,
 
     # Get the database.
     with contextlib.closing(config.get_database(database)) as db:
+        session = db.make_session()
 
         # Get the testsuite.
         ts = db.testsuite[testsuite]
@@ -251,7 +253,7 @@ def action_send_daily_report(instance_path, address, database, testsuite, host,
             date = datetime.datetime.utcnow()
         else:
             # Get a timestamp to use to derive the daily report to generate.
-            latest = ts.query(ts.Run).\
+            latest = session.query(ts.Run).\
                 order_by(ts.Run.start_time.desc()).limit(1).first()
 
             # If we found a run, use it's start time (rounded up to the next
@@ -269,7 +271,7 @@ def action_send_daily_report(instance_path, address, database, testsuite, host,
             day_start_offset_hours=date.hour, for_mail=True,
             num_prior_days_to_include=days,
             filter_machine_regex=filter_machine_regex)
-        report.build()
+        report.build(session)
 
         logger.info("generating HTML report...")
         ts_url = "%s/db_%s/v4/%s" \
@@ -337,6 +339,7 @@ def action_send_run_comparison(instance_path, run_a_id, run_b_id, database,
 
     # Get the database.
     with contextlib.closing(config.get_database(database)) as db:
+        session = db.make_session()
 
         # Get the testsuite.
         ts = db.testsuite[testsuite]
@@ -344,9 +347,9 @@ def action_send_run_comparison(instance_path, run_a_id, run_b_id, database,
         # Lookup the two runs.
         run_a_id = int(run_a_id)
         run_b_id = int(run_b_id)
-        run_a = ts.query(ts.Run).\
+        run_a = session.query(ts.Run).\
             filter_by(id=run_a_id).first()
-        run_b = ts.query(ts.Run).\
+        run_b = session.query(ts.Run).\
             filter_by(id=run_b_id).first()
         if run_a is None:
             logger.error("invalid run ID %r (not in database)" % (run_a_id,))
@@ -355,8 +358,8 @@ def action_send_run_comparison(instance_path, run_a_id, run_b_id, database,
 
         # Generate the report.
         data = lnt.server.reporting.runs.generate_run_data(
-            run_b, baseurl=config.zorgURL, result=None, compare_to=run_a,
-            baseline=None, aggregation_fn=min)
+            session, run_b, baseurl=config.zorgURL, result=None,
+            compare_to=run_a, baseline=None, aggregation_fn=min)
 
         env = lnt.server.ui.app.create_jinja_environment()
         text_template = env.get_template('reporting/run_report.txt')

lnt/lnttool/updatedb.py
@@ -34,21 +34,23 @@ def action_updatedb(instance_path, database, testsuite, tmp_dir, show_sql,
 
     # Get the database and test suite.
     with contextlib.closing(instance.get_database(database)) as db:
+        session = db.make_session()
         ts = db.testsuite[testsuite]
         order = None
         # Compute a list of all the runs to delete.
         if delete_order:
-            runs = ts.query(ts.Run).join(ts.Order) \
+            runs = session.query(ts.Run).join(ts.Order) \
                 .filter(ts.Order.id == delete_order).all()
         else:
-            runs = ts.query(ts.Run).filter(ts.Run.id.in_(delete_runs)).all()
+            runs = session.query(ts.Run) \
+                .filter(ts.Run.id.in_(delete_runs)).all()
         for run in runs:
-            ts.delete(run)
+            session.delete(run)
 
         if delete_machines:
-            machines = ts.query(ts.Machine) \
+            machines = session.query(ts.Machine) \
                 .filter(ts.Machine.name.in_(delete_machines)).all()
             for machine in machines:
-                ts.delete(machine)
+                session.delete(machine)
 
-        db.commit()
+        session.commit()

lnt/lnttool/viewcomparison.py
@@ -83,10 +83,11 @@ def action_view_comparison(report_a, report_b, hostname, port, dry_run,
 
         # Import the two reports.
         with contextlib.closing(config.get_database('default')) as db:
-            r = import_and_report(config, 'default', db, report_a, '<auto>',
-                                  testsuite, updateMachine=True)
-            import_and_report(config, 'default', db, report_b, '<auto>',
-                              testsuite, updateMachine=True)
+            session = db.make_session()
+            r = import_and_report(config, 'default', db, session, report_a,
+                                  '<auto>', testsuite, updateMachine=True)
+            import_and_report(config, 'default', db, session, report_b,
+                              '<auto>', testsuite, updateMachine=True)
 
             # Dispatch another thread to start the webbrowser.
             comparison_url = '%s/v4/nts/2?compare_to=1' % (url,)

lnt/server/db/regression.py
@@ -35,26 +35,26 @@ ChangeRuns = namedtuple("ChangeRuns", ["before", "after"])
 ChangeData = namedtuple("ChangeData", ["ri", "cr", "run", "latest_cr"])
 
 
-def new_regression(ts, field_changes):
+def new_regression(session, ts, field_changes):
     """Make a new regression and add to DB."""
     today = datetime.date.today()
     MSG = "Regression of 0 benchmarks"
     title = MSG
     regression = ts.Regression(title, "", RegressionState.DETECTED)
-    ts.add(regression)
+    session.add(regression)
     for fc_id in field_changes:
-        fc = get_fieldchange(ts, fc_id)
+        fc = get_fieldchange(session, ts, fc_id)
         ri1 = ts.RegressionIndicator(regression, fc)
-        ts.add(ri1)
-    rebuild_title(ts, regression)
-    ts.commit()
+        session.add(ri1)
+    rebuild_title(session, ts, regression)
+    session.commit()
     return regression
 
 
-def rebuild_title(ts, regression):
+def rebuild_title(session, ts, regression):
     """Update the title of a regresson."""
     if re.match("Regression of \d+ benchmarks.*", regression.title):
-        old_changes = ts.query(ts.RegressionIndicator) \
+        old_changes = session.query(ts.RegressionIndicator) \
             .filter(ts.RegressionIndicator.regression_id == regression.id) \
             .all()
         new_size = len(old_changes)
@@ -70,66 +70,68 @@ def rebuild_title(ts, regression):
     return regression
 
 
-def get_all_orders_for_machine(ts, machine):
+def get_all_orders_for_machine(session, ts, machine):
     """Get all the oredrs for this sa machine."""
-    return ts.query(ts.Order) \
+    return session.query(ts.Order) \
         .join(ts.Run) \
         .filter(ts.Run.machine_id == machine) \
         .order_by(asc(ts.Order.llvm_project_revision)) \
         .all()
 
 
-def get_ris(ts, regression_id):
-    return ts.query(ts.RegressionIndicator) \
+def get_ris(session, ts, regression_id):
+    return session.query(ts.RegressionIndicator) \
         .filter(ts.RegressionIndicator.regression_id == regression_id) \
         .all()
 
 
-def get_runs_for_order_and_machine(ts, order_id, machine_id):
+def get_runs_for_order_and_machine(session, ts, order_id, machine_id):
     """Collect all the runs for a particular order/machine combo."""
-    runs = ts.query(ts.Run) \
+    runs = session.query(ts.Run) \
         .filter(ts.Run.machine_id == machine_id) \
         .filter(ts.Run.order_id == order_id) \
         .all()
     return runs
 
 
-def get_runs_of_fieldchange(ts, fc):
-    before_runs = get_runs_for_order_and_machine(ts, fc.start_order_id,
+def get_runs_of_fieldchange(session, ts, fc):
+    before_runs = get_runs_for_order_and_machine(session, ts,
+                                                 fc.start_order_id,
                                                  fc.machine_id)
-    after_runs = get_runs_for_order_and_machine(ts, fc.end_order_id,
+    after_runs = get_runs_for_order_and_machine(session, ts, fc.end_order_id,
                                                 fc.machine_id)
     return ChangeRuns(before_runs, after_runs)
 
 
-def get_current_runs_of_fieldchange(ts, fc):
-    before_runs = get_runs_for_order_and_machine(ts, fc.start_order_id,
+def get_current_runs_of_fieldchange(session, ts, fc):
+    before_runs = get_runs_for_order_and_machine(session, ts,
+                                                 fc.start_order_id,
                                                  fc.machine_id)
-    newest_order = get_all_orders_for_machine(ts, fc.machine_id)[-1]
+    newest_order = get_all_orders_for_machine(session, ts, fc.machine_id)[-1]
 
-    after_runs = get_runs_for_order_and_machine(ts, newest_order.id,
+    after_runs = get_runs_for_order_and_machine(session, ts, newest_order.id,
                                                 fc.machine_id)
     return ChangeRuns(before_runs, after_runs)
 
 
-def get_first_runs_of_fieldchange(ts, fc):
-    run = ts.query(ts.Run) \
+def get_first_runs_of_fieldchange(session, ts, fc):
+    run = session.query(ts.Run) \
         .filter(ts.Run.machine_id == fc.machine_id) \
         .filter(ts.Run.order_id == fc.end_order_id) \
         .first()
     return run
 
 
-def get_cr_for_field_change(ts, field_change, current=False):
+def get_cr_for_field_change(session, ts, field_change, current=False):
     """Given a filed_change, calculate a comparison result for that change.
     And the last run."""
     if current:
-        runs = get_current_runs_of_fieldchange(ts, field_change)
+        runs = get_current_runs_of_fieldchange(session, ts, field_change)
     else:
-        runs = get_runs_of_fieldchange(ts, field_change)
+        runs = get_runs_of_fieldchange(session, ts, field_change)
     runs_all = list(runs.before)
     runs_all.extend(runs.after)
-    ri = RunInfo(ts, [r.id for r in runs_all],
+    ri = RunInfo(session, ts, [r.id for r in runs_all],
                  only_tests=[field_change.test_id])
     cr = ri.get_comparison_result(runs.after, runs.before,
                                   field_change.test.id, field_change.field,
@@ -137,6 +139,7 @@ def get_cr_for_field_change(ts, field_change, current=False):
     return cr, runs.after[0], runs_all
 
 
-def get_fieldchange(ts, fc_id):
+def get_fieldchange(session, ts, fc_id):
     """Get a fieldchange given an ID."""
-    return ts.query(ts.FieldChange).filter(ts.FieldChange.id == fc_id).one()
+    return session.query(ts.FieldChange) \
+        .filter(ts.FieldChange.id == fc_id).one()

lnt/server/db/rules/rule_blacklist_benchmarks_by_name.py
@@ -30,7 +30,7 @@ def _populate_blacklist():
         logger.warning("Ignoring blacklist file: {}".format(path))
 
 
-def filter_by_benchmark_name(ts, field_change):
+def filter_by_benchmark_name(session, ts, field_change):
     """Is this a fieldchanges we care about?
     """
     if ignored is None:

commit 01cbd1167d69764a49a58099a7e0bfecf114a774

Further fixes for fieldchange.
Turns out we have not enough tests for the fieldchange logic so I
accidentally broke some of it in r312363.

git-svn-id: https://llvm.org/svn/llvm-project/lnt/trunk@312588 91177308-0d34-0410-b5e6-96231b3b80d8

lnt/server/db/fieldchange.py
@@ -16,7 +16,7 @@ FIELD_CHANGE_LOOKBACK = 10
 
 
 def post_submit_tasks(session, ts, run_id):
-    regenerate_fieldchanges_for_run(ts, session, run_id)
+    regenerate_fieldchanges_for_run(session, ts, run_id)
 
 
 def delete_fieldchange(session, ts, change):
@@ -55,7 +55,7 @@ def delete_fieldchange(session, ts, change):
 
 
 @timed
-def regenerate_fieldchanges_for_run(ts, session, run_id):
+def regenerate_fieldchanges_for_run(session, ts, run_id):
     """Regenerate the set of FieldChange objects for the given run.
     """
     # Allow for potentially a few different runs, previous_runs, next_runs
@@ -129,6 +129,7 @@ def regenerate_fieldchanges_for_run(ts, session, run_id):
                                    machine=run.machine,
                                    test=test,
                                    field_id=field.id)
+                f.field = field
                 # Check the rules to see if this change matters.
                 if rules.is_useful_change(session, ts, f):
                     session.add(f)

lnt/util/async_ops.py
@@ -134,7 +134,7 @@ def async_wrapper(job, ts_args, func_args):
         with contextlib.closing(db):
             session = db.make_session()
             ts = db.testsuite[ts_args['tsname']]
-            nothing = job(ts, session, **func_args)
+            nothing = job(session, ts, **func_args)
             assert nothing is None
             session.close()
         end_time = time.time()